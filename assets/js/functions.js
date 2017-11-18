5fc6e42d08e180e46413c4745f3dd4a41a172cac
User 1 <you@example.com>
2017-11-18 17:06:13 +0100

    76

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,3 +8,44 @@ $(document).ready(function(){
     $(".hamburger-menu").toggleClass("open");
   });
 });
+$(document).ready(function(){
+  $(".hamburger").click(function(){
+    $(".links2").toggleClass("open2");
+  });
+});
+$(document).ready(function() {
+    $('body').hide().fadeIn(500);
+});
+
+var $about = $('.about');
+
+$about.waypoint(function (direction) {
+  if (direction == 'down') {
+    $about.addClass('plus');
+  }
+  else {
+    $about.removeClass('plus');
+  }
+}, { offset: '50%' });
+
+var $news = $('.newsletter');
+
+$news.waypoint(function (direction) {
+  if (direction == 'down') {
+    $news.addClass('plus');
+  }
+  else {
+    $news.removeClass('plus');
+  }
+}, { offset: '50%' });
+
+var $footer = $('.footer');
+
+$footer.waypoint(function (direction) {
+  if (direction == 'down') {
+    $footer.addClass('plus');
+  }
+  else {
+    $footer.removeClass('plus');
+  }
+}, { offset: '80%' });
